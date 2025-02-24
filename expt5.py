e6e6affbcd17ab5a06eaabc8eb9f440e82cb63d7
tejasbhosale112 <bhosaletejas436@gmail.com>
2025-02-24 12:46:30 +0530

    Add files via upload

---
+++ expt5.py
@@ -0,0 +1,8 @@
+#to find standard deviation and variance for given data set 
+import statistics
+data=[10,12,15,20,22,24,28,30]
+sd=statistics.stdev(data)
+var=statistics.variance(data)
+print("the standard deviation is:",sd)
+print("the variance is:",var)
+    
